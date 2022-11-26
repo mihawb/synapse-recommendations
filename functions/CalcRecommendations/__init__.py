505e0b2db5f8f70a81d5d08c360f63b609896fb5
mihawb <michalbanaszczak@gmail.com>
2022-11-26 14:13:04 +0100

    polaczenie z baza

--- functions/CalcRecommendations/__init__.py
+++ functions/CalcRecommendations/__init__.py
@@ -1,24 +1,23 @@
 import logging
-
 import azure.functions as func
+import os
+import pyodbc
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    connection_string = os.getenv('FUNC_TO_DB_CONNECTION_STRING')
+    rows = []
+    with pyodbc.connect(connection_string) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT * FROM SPOTIFY_DATA WHERE ARTISTS='Drake'")
+            row = cursor.fetchone()
+            while row:
+                rows.append(row)
+                row = cursor.fetchone()
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    if rows == []:
+        return func.HttpResponse('nic nie znaleziono', status_code=200)
+    else: 
+        return func.HttpResponse(str(rows), status_code=200)
