27bd1adda72e86857d583e2790dd928204e5f86f
Daniel Stańkowski <daniostan909v2@gmail.com>
2022-11-27 23:29:44 +0100

    Links for recommended songs added

--- frontend/src/Song.js
+++ frontend/src/Song.js
@@ -4,6 +4,7 @@ import './Song.css'
 
 const Song = (props) => {
 	return (
+		<a href={props.data.external_urls.spotify} className="nostyle">
 		<div className='song-outer'>
 			<img className='album-cover' src={props.data.album.images[0].url} alt='album cover'/>
 			<div className='song-info'>
@@ -11,6 +12,7 @@ const Song = (props) => {
 				<p className='secondary-info'>{props.data.artists.map(a => a.name).join(', ')} • {props.data.album.name}</p>
 			</div>
 		</div>
+		</a>
 	)
 }
 
